418cccd011ecaf066d95d35c664d50ccc7131f90
cedric.walter <cedric.walter@gmail.com>
2017-08-07 21:19:20 +0200

    add comments

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    # where the openssl line is the one you took note of earlier when encrypting the keys
     openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
     gpg --fast-import cd/signingkey.asc
 fi
